a6d407cb98dc12ade9a49397073c1a43edc8ea6f
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2021-12-29 05:30:16 +0000

    Update pokemonsprite.py

--- pokemonsprite.py
+++ pokemonsprite.py
@@ -1,4 +1,5 @@
 import os
+import pygame.image
 import xml.etree.ElementTree as ET
 
 class PokemonSprite:
@@ -10,10 +11,35 @@ class PokemonSprite:
         self.load_all_sprites()
 
     def get_root(self):
-        directory = os.path.join(PokemonSprite.SPRITE_DIRECTORY, self.poke_id, "AnimData.xml")
+        directory = os.path.join(self.get_directory(), "AnimData.xml")
         tree = ET.parse(directory)
         return tree.getroot()
 
-    def load_all_sprites(self):
-        pass
+    def get_directory(self):
+        return os.path.join(PokemonSprite.SPRITE_DIRECTORY, self.poke_id)
 
+    def load_all_sprites(self):
+        self.root = self.get_root()
+        self.sprite_dict = {}
+        for anim in self.root.find("Anims").findall("Anim"):
+            name = anim.find("Name").text
+            file_name = name + "-Anim.png"
+            self.sprite_dict[name] = pygame.image.load(os.path.join(self.get_directory(), file_name))
+
+    def get_sprite_frame_size(self, name):
+        for anim in self.root.find("Anims").findall("Anim"):
+            if anim.find("Name").text == name:
+                width = anim.find("FrameWidth")
+                height = anim.find("FrameHeight")
+                return (width, height)
+
+    def get_sprite_durations(self, name):
+        for anim in self.root.find("Anims").findall("Anim"):
+            if anim.find("Name").text == name:
+                durations = []
+                for d in anim.find("Durations").findall("Duration"):
+                    durations.append(int(d.text))
+                return durations
+
+    def get_sprite(self, name, direction, frame):
+        pass
