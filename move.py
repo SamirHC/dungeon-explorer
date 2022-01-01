0afd51135c448b8d1a784b43bfa6eb383f221c24
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-01-01 19:55:45 +0000

    Updated Move class, added MoveCategory enum, added MoveEffect class

--- move.py
+++ move.py
@@ -1,31 +1,44 @@
-import configparser
+import enum
 import os
 import damage_chart
 
+import xml.etree.ElementTree as ET
+
 class Move:
-    MOVE_DIRECTORY = os.path.join(os.getcwd(), "GameData", "Moves")
+    MOVE_DIRECTORY = os.path.join(os.getcwd(), "GameData", "new_moves")
 
-    def __init__(self, name: str):
-        self.name = name
-        self.load_move_data()
-        # Single
-        
-    def load_move_data(self):
-        config = configparser.RawConfigParser()
-        data = os.path.join(Move.MOVE_DIRECTORY, self.name, "moveData.cfg")
-        config.read(data)
-        section = config.sections()[0]
-        self.power = [int(x) for x in config.get(section, "power").split(",")]
-        self.accuracy = [int(x) for x in config.get(section, "accuracy").split(",")]
-        self.critical = int(config.get(section, "critical"))
-        self.pp = int(config.get(section, "pp"))
-        self.type = damage_chart.Type(int(config.get(section, "type")))
-        self.category = config.get(section, "category")  # ["ATK","SPATK"]
-        self.cuts_corners = int(config.get(section, "cutscorners"))  # 1/0 [True/False]
-        # Multi
-        self.target_type = config.get(section, "targettype").split(",")  # ["Self","Allies","Enemies","All"]
-        self.ranges = config.get(section, "ranges").split(",")
-        self.effects = config.get(section, "effects").split(",")  # ["Damage","Heal","ATK+","DEF+","SPATK+","SPDEF+","ATK-","DEF-","SPATK-","SPDEF-"...]
+    def __init__(self, move_id: str):
+        self.move_id = move_id
+        self.parse_file()
 
-    def empty_pp(self):
-        self.pp = 0
+    def parse_file(self):
+        file = os.path.join(self.MOVE_DIRECTORY, self.move_id + ".xml")
+        tree = ET.parse(file)
+        self.root = tree.getroot()
+        self.name = self.root.find("Name").text
+        self.type = damage_chart.Type(self.root.find("Type").text)
+        self.category = self.root.find("Category").text
+        self.pp = int(self.root.find("PP").text)
+        self.accuracy = int(self.root.find("Accuracy").text)
+        self.critical = int(self.root.find("Critical").text)
+        self.effects = []
+        for effect_element in self.root.find("Effects").findall("Effect"):
+            self.effects.append(MoveEffect(effect_element))
+
+class MoveCategory(enum.Enum):
+    PHYSICAL = 0
+    SPECIAL = 1
+    STATUS = 2
+
+class MoveEffect:
+    def __init__(self, root: ET.Element):
+        self.root = root
+        self.parse()
+
+    def parse(self):
+        self.target = self.root.find("Target").text
+        self.animation = self.root.find("Animation").text
+        self.effect_type = self.root.find("EffectType").text
+        self.power = int(self.root.find("Power").text)
+        self.cuts_corners = int(self.root.find("CutsCorners").text)
+        self.range_category = int(self.root.find("RangeCategory").text)
