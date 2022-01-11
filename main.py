b2e4982653c520ebced7d5e43685bda420371f85
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-01-11 16:52:31 +0000

    if name == main

--- main.py
+++ main.py
@@ -4,47 +4,46 @@ import pygame
 import pygame.display
 import pygame.event
 import pygame.time
-import scene
 import scenemanager
 
-# Initialisation
-pygame.init()
-display = pygame.display.set_mode(constants.DISPLAY_SIZE)
-pygame.display.set_caption(constants.CAPTION)
 
-clock = pygame.time.Clock()
-input_stream = inputstream.InputStream()
-scene_manager = scenemanager.SceneManager()
-#dungeon_id = "BeachCave"
-#user_id = "0025"
-#dungeon_scene = scene.DungeonScene(dungeon_id, user_id)
-#main_menu_scene = scene.MainMenuScene()
-#scene_manager.add(dungeon_scene)
-#scene_manager.add(main_menu_scene)
+def main():
+    # Initialisation
+    pygame.init()
+    display = pygame.display.set_mode(constants.DISPLAY_SIZE)
+    pygame.display.set_caption(constants.CAPTION)
 
-# Game loop
-running = True
-while running:
-    # Gets the keyboard state
-    input_stream.update()
+    clock = pygame.time.Clock()
+    input_stream = inputstream.InputStream()
+    scene_manager = scenemanager.SceneManager()
 
-    # Checks if user quits
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
+    # Game loop
+    running = True
+    while running:
+        # Gets the keyboard state
+        input_stream.update()
 
-    # Toggle Fullscreen
-    if input_stream.keyboard.is_pressed(pygame.K_F11):
-        if display.get_flags() & pygame.FULLSCREEN:
-            pygame.display.set_mode(constants.DISPLAY_SIZE)
-        else:
-            pygame.display.set_mode(constants.DISPLAY_SIZE,
-                                    pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF)
+        # Checks if user quits
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                running = False
 
-    scene_manager.process_input(input_stream)
-    scene_manager.update()
-    scene_manager.render(display)
+        # Toggle Fullscreen
+        if input_stream.keyboard.is_pressed(pygame.K_F11):
+            if display.get_flags() & pygame.FULLSCREEN:
+                pygame.display.set_mode(constants.DISPLAY_SIZE)
+            else:
+                pygame.display.set_mode(constants.DISPLAY_SIZE,
+                                        pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF)
 
-    clock.tick(constants.FPS)
+        scene_manager.process_input(input_stream)
+        scene_manager.update()
+        scene_manager.render(display)
 
-pygame.quit()
+        clock.tick(constants.FPS)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
