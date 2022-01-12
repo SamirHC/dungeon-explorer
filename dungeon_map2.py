a99773a050518098bbf39e9d07673c22058f94d0
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-01-12 12:42:01 +0000

    FloorData class, pulls data from xml

---
+++ dungeon_map2.py
@@ -0,0 +1,14 @@
+import dungeon_map
+import os
+import xml.etree.ElementTree as ET
+
+
+class FloorData:
+    def __init__(self, dungeon_id, floor_number):
+        self.dungeon_id = dungeon_id
+        self.floor_number = floor_number
+        file = os.path.join(os.getcwd(), "GameData", "Dungeons", f"{dungeon_id}.xml")
+        tree = ET.parse(file)
+        self.root = tree.getroot().findall("Floor")[floor_number]
+
+
