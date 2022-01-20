5537418be7a3c6be99b8081b9fdc75b67294a292
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-01-20 11:22:22 +0000

    New FloorBuilder2 class. Normal floor generation algorithm in progress

--- dungeon_map.py
+++ dungeon_map.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import generatordata
 import os
 import pygame
 import random
@@ -231,6 +232,303 @@ class OutdatedFloorBuilder(FloorBuilder):
     def _is_west_exit(self, position: tuple[int, int]):
         x, y = position
         return self._floor.is_ground((x - 1, y)) and not self._floor.is_ground((x - 1, y - 1)) and not self._floor.is_ground((x - 1, y + 1))
+
+
+class FloorBuilder2(FloorBuilder):
+    def __init__(self, dungeon_id, floor_number):
+        self.dungeon_id = dungeon_id
+        self.floor_number = floor_number
+        self.floor_size = 0
+        self.data = self.load_generator_data()
+
+    class Cell:
+        def __init__(self):
+            self.start_x = 0
+            self.start_y = 0
+            self.end_x = 0
+            self.end_y = 0
+            self.valid_cell = False
+            self.unk1 = 0
+            self.is_room = False
+            self.is_connected = 0
+            self.unk2 = 0
+            self.unk3 = 0
+            self.is_mh = 0
+            self.unk4 = 0
+            self.is_maze = 0
+            self.has_been_merged = 0
+            self.is_merged = 0
+            self.connections: set[direction.Direction] = set()
+            self.connected_to_top_2 = 0
+            self.connected_to_bottom_2 = 0
+            self.connected_to_left_2 = 0
+            self.connected_to_right_2 = 0
+            self.unk5 = 0
+            self.imperfect = False
+            self.secondary = False
+
+    def load_generator_data(self):
+        return generatordata.FloorGeneratorData(self.dungeon_id, self.floor_number)
+
+    def build_floor(self) -> Floor:
+        self.floor = super().build_floor()
+
+        if self.data.fixed_floor_id != "0":
+            return self.build_fixed_floor()
+
+        self.generate_floor_structure()
+        return self.floor
+
+    def build_fixed_floor(self):
+        pass
+
+    def generate_floor_structure(self):
+        s = self.data.structure
+        if s == "SMALL":
+            self.grid_size = (4, random.randrange(2)+2)
+            self.floor_size = 1
+            return self.generate_normal_floor()
+        if s == "ONE_ROOM_MH":
+            return None
+        if s == "RING":
+            return self.generate_ring()
+        if s == "CROSSROADS":
+            return self.generate_crossroads()
+        if s == "TWO_ROOMS_MH":
+            return None
+        if s == "LINE":
+            return self.generate_line()
+        if s == "CROSS":
+            return self.generate_cross()
+        if s == "BETTLE":  # Typo: To be changed to BEETLE
+            return self.generate_beetle()
+        if s == "OUTER_ROOMS":
+            return None
+        if s == "MEDIUM":
+            self.grid_size = 4, random.randrange(2)+2
+            self.floor_size = 2
+            return self.generate_normal_floor()
+        if s == "SMALL_MEDIUM":
+            self.grid_size = random.randrange(2, 5), random.randrange(2, 4)
+        else:
+            self.grid_size = random.randrange(2, 7), random.randrange(2, 5)
+            return self.generate_normal_floor()
+
+    def generate_normal_floor(self):
+        self.grid = self.init_grid()
+        self.grid_xs, self.grid_ys = self.grid_positions()
+        self.assign_rooms()
+        self.create_rooms()
+        self.connect_cells()
+        self.create_hallways()
+        self.insert_stairs()
+
+    def generate_ring(self):
+        pass
+
+    def generate_crossroads(self):
+        pass
+
+    def generate_line(self):
+        pass
+
+    def generate_cross(self):
+        pass
+
+    def generate_beetle(self):
+        pass
+
+    def grid_positions(self) -> tuple[list[int], list[int]]:
+        cell_size_x = self.floor.WIDTH // self.grid_size[0]
+        cell_size_y = self.floor.HEIGHT // self.grid_size[1]
+        xs = list(range(0, self.floor.WIDTH+1, cell_size_x))
+        ys = list(range(0, self.floor.HEIGHT+1, cell_size_y))
+        return xs, ys
+
+    def init_grid(self) -> dict[tuple[int, int], Cell]:
+        grid = {(x, y):self.Cell() for x in range(self.grid_size[0]) for y in range(self.grid_size[1])}
+        for (x, _), cell in grid.items():
+            if self.floor_size == 1 and x >= self.grid_size[0]//2:
+                cell.valid_cell = False
+            elif self.floor_size == 2 and x >= self.grid_size[0]*3//4:
+                cell.valid_cell = False
+            else:
+                cell.valid_cell = True
+        return grid
+
+    def assign_rooms(self):
+        room_density = self.data.room_density
+        if room_density < 0:
+            room_density = -room_density
+        else:
+            room_density = room_density + random.randrange(0, 3)
+
+        rooms_ok = [(i < room_density) for i in range(len(self.grid))]
+        while True:
+            random.shuffle(rooms_ok)
+            placed = 0
+            for i, cell in enumerate(self.grid.values()):
+                if not cell.valid_cell:
+                    continue
+                if rooms_ok[i]:
+                    cell.is_room = True
+                    placed += 1
+            if placed >= 2:
+                return
+
+    def create_rooms(self):
+        room_number = 1
+        for (x, y), cell in self.grid.items():
+            if not cell.valid_cell:
+                continue
+            x0, x1 = self.grid_xs[x:x+2]
+            y0, y1 = self.grid_ys[y:y+2]
+            max_w = x1 - x0 - 4
+            max_h = y1 - y0 - 3
+            if cell.is_room:
+                w = random.randrange(5, max_w)
+                h = random.randrange(4, max_h)
+                w = min(w, h*3//2)
+                h = min(h, w*3//2)
+                cell.start_x = random.randrange(max_w-w)+x0+2
+                cell.start_y = random.randrange(max_h-h)+y0+2
+                cell.end_x = cell.start_x + w
+                cell.end_y = cell.start_y + h
+                for tile_x in range(cell.start_x, cell.end_x):
+                    for tile_y in range(cell.start_y, cell.end_y):
+                        self.floor[tile_x, tile_y] = tile.Tile.room_tile(room_number)
+                cell.imperfect = random.randrange(100) < self.data.imperfect_rooms
+                cell.secondary = random.randrange(100) < self.data.secondary_percentage
+                room_number += 1
+            else:  # Dummy room
+                l_margin = 1 if x == 0 else 2
+                r_margin = 2 if x == self.grid_size[0]-1 else 4
+                u_margin = 1 if y == 0 else 2
+                b_margin = 2 if y == self.grid_size[1]-1 else 4
+                cell.start_x = random.randrange(x0+2+l_margin, x0+2+max_w-r_margin)
+                cell.start_y = random.randrange(y0+2+u_margin, y0+2+max_h-b_margin)
+                cell.end_x = cell.start_x + 1
+                cell.end_y = cell.start_y + 1
+                self.floor[cell.start_x, cell.start_y] = tile.Tile.hallway_tile()
+
+    def connect_cells(self):
+        position = random.choice([p for p, cell in self.grid.items() if cell.valid_cell])
+        for _ in range(self.data.floor_connectivity):
+            position = self.connect_cell(position)
+        self.remove_dead_ends()
+
+    def connect_cell(self, position: tuple[int, int]):
+        x, y = position
+        ds = direction.Direction.get_non_diagonal_directions()
+        if x == 0:
+            ds.remove(direction.Direction.WEST)
+        elif x == self.grid_size[0]-1 or not self.grid[x+1, y].valid_cell:
+            ds.remove(direction.Direction.EAST)
+        if y == 0:
+            ds.remove(direction.Direction.NORTH)
+        elif y == self.grid_size[1]-1 or not self.grid[x, y+1].valid_cell:
+            ds.remove(direction.Direction.SOUTH)
+        d = random.choice(list(ds))
+        dx, dy = d.value
+        self.grid[x, y].connections.add(d)
+        self.grid[x+dx, y+dy].connections.add(d.flip())
+        return x+dx, y+dy
+    
+    def remove_dead_ends(self):
+        if self.data.dead_ends:
+            return
+        checking_dead_ends = True
+        while checking_dead_ends:
+            checking_dead_ends = False
+            for p, cell in self.grid.items():
+                if not cell.valid_cell or cell.is_room:
+                    continue
+                if len(cell.connections) == 1:
+                    checking_dead_ends = True
+                    self.connect_cell(p)
+
+    def create_hallways(self):
+        seen = []
+        for (x, y), cell in self.grid.items():
+            if not cell.valid_cell:
+                return
+            for d in cell.connections:
+                if (cell, d) in seen:
+                    continue
+                self.create_hallway((x, y), d)
+                seen.append((cell, d))
+                dx, dy = d.value
+                seen.append((self.grid[x+dx, y+dy], d.flip()))
+
+    def create_hallway(self, cell_position: tuple[int, int], d: direction.Direction):
+        cell = self.grid[cell_position]
+        if not cell.is_room:
+            x0, y0 = cell.start_x, cell.start_y
+        else:
+            x0 = random.randrange(cell.start_x+1, cell.end_x-1)
+            y0 = random.randrange(cell.start_y+1, cell.end_y-1)
+        x, y = cell_position
+        dx, dy = d.value
+        other_cell = self.grid[x+dx, y+dy]
+        if not other_cell.is_room:
+            x1 = other_cell.start_x
+            y1 = other_cell.start_y
+        else:
+            x1 = random.randrange(other_cell.start_x+1, other_cell.end_x-1)
+            y1 = random.randrange(other_cell.start_y+1, other_cell.end_y-1)
+        cur_x, cur_y = x0, y0
+        if d.is_horizontal():
+            if d == direction.Direction.EAST:
+                border = self.grid_xs[x+1]
+            else:
+                border = self.grid_xs[x]
+            while cur_x != border:
+                if not self.floor[cur_x, cur_y].room_index:
+                    self.floor[cur_x, cur_y] = tile.Tile.hallway_tile()
+                cur_x += dx
+            while cur_y != y1:
+                if self.floor[cur_x, cur_y].terrain == tile.Terrain.GROUND:
+                    return
+                self.floor[cur_x, cur_y] = tile.Tile.hallway_tile()
+                if cur_y >= y1:
+                    cur_y -= 1
+                else:
+                    cur_y += 1
+            while cur_x != x1:
+                if self.floor[cur_x, cur_y].terrain == tile.Terrain.GROUND:
+                    return
+                self.floor[cur_x, cur_y] = tile.Tile.hallway_tile()
+                cur_x += dx
+        elif d.is_vertical():
+            if d == direction.Direction.SOUTH:
+                border = self.grid_ys[y+1]
+            else:
+                border = self.grid_ys[y]
+            while cur_y != border:
+                if not self.floor[cur_x, cur_y].room_index:
+                    self.floor[cur_x, cur_y] = tile.Tile.hallway_tile()
+                cur_y += dy
+            while cur_x != x1:
+                if self.floor[cur_x, cur_y].terrain == tile.Terrain.GROUND:
+                    return
+                self.floor[cur_x, cur_y] = tile.Tile.hallway_tile()
+                if cur_x >= x1:
+                    cur_x -= 1
+                else:
+                    cur_x += 1
+            while cur_y != y1:
+                if self.floor[cur_x, cur_y].terrain == tile.Terrain.GROUND:
+                    return
+                self.floor[cur_x, cur_y] = tile.Tile.hallway_tile()
+                cur_y += dy
+        cell.is_connected = True
+        other_cell.is_connected = True
+
+    def merge_rooms(self):
+        pass
+
+    def insert_stairs(self):
+        self.floor.stairs_spawn = (5, 5)
 
 
 class Floor:
