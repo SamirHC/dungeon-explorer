020b1a62b8aec85c3179f2b8ee76bb52b4fd2860
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-02-04 18:39:40 +0000

    Defender animation should reset upon iteration

--- battlesystem.py
+++ battlesystem.py
@@ -260,6 +260,8 @@ class BattleSystem:
             self.index += 1
         if self.attacker is not None and self.attacker.animation.iterations:
             self.attacker.animation_name = "Idle"
+        if self.defender is not None and self.defender.hp > 0 and self.defender.animation.iterations:
+            self.defender.animation_name = "Idle"
 
     def handle_event(self, event_type, event_data):
         if event_type == "LogEvent":
