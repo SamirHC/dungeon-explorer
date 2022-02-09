69abdace1cec4f74abd551d7530d095c6ec12dbe
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-02-09 12:46:11 +0000

    Update tileset.py

--- dungeon_explorer/dungeon/tileset.py
+++ dungeon_explorer/dungeon/tileset.py
@@ -2,6 +2,7 @@ import os
 from . import pattern, tile
 import pygame
 import pygame.image
+import xml.etree.ElementTree as ET
 
 
 class TileSet:
@@ -13,33 +14,30 @@ class TileSet:
     SHOP_IMAGE = pygame.image.load(os.path.join(
         os.getcwd(), "assets", "images", "traps", "KecleonCarpet.png"))
 
-    def __init__(self, dungeon_id: str):
-        self.dungeon_id = dungeon_id
+    def __init__(self, tileset_id: str):
+        self.tileset_id = tileset_id
+
+        base_dir = os.path.join(TileSet.TILE_SET_DIR, tileset_id)
+        self.metadata = ET.parse(os.path.join(base_dir, "tileset.dtef.xml")).getroot()
+        self.tile_size = int(self.metadata.get("dimensions"))
+
         self.tile_set: list[pygame.Surface] = []
         for i in range(3):
-            tile_set_dir = os.path.join(
-                TileSet.TILE_SET_DIR, dungeon_id, f"tileset_{i}.png")
-            self.tile_set.append(pygame.image.load(tile_set_dir))
+            self.tile_set.append(pygame.image.load(os.path.join(base_dir, f"tileset_{i}.png")))
 
-    def get_tile_size(self) -> int:
-        return self.tile_set[0].get_width() // 18
-
-    def get_tile_surface(self, terrain: tile.Terrain, pattern: pattern.Pattern, variation: int) -> pygame.Surface:
-        tile_surface = self.tile_set[variation].subsurface(
-            self.get_rect(terrain, pattern))
-        return tile_surface
+    def get_tile_surface(self, terrain: tile.Terrain, pattern: pattern.Pattern, variation: int=0) -> pygame.Surface:
+        return self.tile_set[variation].subsurface(self.get_rect(terrain, pattern))
 
     def get_rect(self, terrain: tile.Terrain, pattern: pattern.Pattern) -> pygame.Rect:
-        side_length = self.get_tile_size()
         x, y = self.get_position(terrain, pattern)
-        return pygame.Rect((x * self.get_tile_size(), y * self.get_tile_size()), (side_length, side_length))
+        return pygame.Rect((x * self.tile_size, y * self.tile_size), (self.tile_size, self.tile_size))
 
     def get_position(self, terrain: tile.Terrain, pattern: pattern.Pattern) -> tuple[int, int]:
         x, y = pattern.get_position()
         return (x + 6 * terrain.value, y)
 
     def get_border_tile(self) -> pygame.Surface:
-        return self.get_tile_surface(tile.Terrain.WALL, pattern.Pattern.border_pattern(), 0)
+        return self.get_tile_surface(tile.Terrain.WALL, pattern.Pattern.border_pattern())
 
     def get_stair_tile(self) -> pygame.Surface:
         return self.STAIRS_IMAGE
