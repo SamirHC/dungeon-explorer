9f4336c482996e9fceab6d9cd7562ee421eb57c0
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-02-18 16:21:53 +0000

    Create party.py

---
+++ dungeon_explorer/pokemon/party.py
@@ -0,0 +1,30 @@
+from . import pokemon
+
+
+class Party:
+    MAX_MEMBERS = 4
+
+    def __init__(self):
+        self.party = []
+        self.id_list = []
+
+    def __len__(self) -> int:
+        return len(self.party)
+
+    def __getitem__(self, index) -> pokemon.UserPokemon:
+        return self.party[index]
+
+    @property
+    def user(self) -> pokemon.UserPokemon:
+        return self.party[0]
+
+    def add(self, user_id: str):
+        if len(self) < Party.MAX_MEMBERS:
+            self.party.append(pokemon.UserPokemon(user_id))
+            self.id_list.append(user_id)
+
+    def remove(self, user_id: str):
+        self.party.pop(self.id_list.index(user_id))
+
+    def is_defeated(self) -> bool:
+        return not self.party
