b45571bdefa9e0491260922ce210847898916c7b
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-02-19 23:58:45 +0000

    HudComponents class

--- dungeon_explorer/dungeon/hud.py
+++ dungeon_explorer/dungeon/hud.py
@@ -10,6 +10,36 @@ pygame.display.init()
 component_color = constants.ORANGE
 HUD_COMPONENTS_FILE = os.path.join(
     os.getcwd(), "assets", "images", "misc", "hud_components.png")
+class HudComponents:
+    HUD_COMPONENTS_FILE = os.path.join(
+        os.getcwd(), "assets", "images", "misc", "hud_components.png")
+    SIZE = 8
+
+    def __init__(self):
+        self.hud_components = pygame.image.load(self.HUD_COMPONENTS_FILE)
+        self.hud_components.set_colorkey(self.hud_components.get_at((0, 0)))
+
+    def __getitem__(self, position: tuple[int, int]):
+        x, y = position
+        return self.hud_components.subsurface(x*self.SIZE, y*self.SIZE, self.SIZE, self.SIZE)
+
+    def get_f(self) -> pygame.Surface:
+        return self[10, 0]
+
+    def get_l(self) -> pygame.Surface:
+        return self[11, 0]
+
+    def get_v(self) -> pygame.Surface:
+        return self[12, 0]
+
+    def get_b(self) -> pygame.Surface:
+        return self[13, 1]
+
+    def get_h(self) -> pygame.Surface:
+        return self[10, 1]
+
+    def get_p(self) -> pygame.Surface:
+        return self[11, 1]
 
 hud_components = pygame.image.load(HUD_COMPONENTS_FILE)
 hud_components.set_colorkey(hud_components.get_at((0, 0)))
