ffdd20022ee66e7cb92c8b6dce4babff0df7aebb
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-03-04 14:26:02 +0000

    BannerFont class and assets added

--- dungeon_explorer/common/text.py
+++ dungeon_explorer/common/text.py
@@ -2,9 +2,44 @@ import os
 
 import pygame
 import pygame.font
+import pygame.image
+import xml.etree.ElementTree as ET
 from dungeon_explorer.common import constants
 
 pygame.font.init()
+
+class BannerFont:
+    def __init__(self):
+        self.source = pygame.image.load(os.path.join("assets", "font", "banner", "banner.png"))
+        self.metadata = ET.parse(os.path.join(os.path.join("assets", "font", "banner", "banner.xml"))).getroot()
+        self.size = 24
+
+    def __getitem__(self, char: str) -> pygame.Surface:
+        char_id = ord(char)
+        x = (char_id % 16) * self.size
+        y = (char_id // 16) * self.size
+        w, h = self.get_width(char), self.size
+        return self.source.subsurface((x, y, w, h))
+
+    def get_width(self, char: str) -> int:
+        char_id = ord(char)
+        elements = self.metadata.find("Table").findall("Char")
+        for el in elements:
+            if int(el.get("id")) == char_id:
+                return int(el.get("width"))
+        return 0
+
+    def build(self, message: str) -> pygame.Surface:
+        chars = [self[c] for c in message]
+        surface = pygame.Surface((sum([c.get_width() for c in chars]), self.size))
+        x = 0
+        for c in chars:
+            surface.blit(c, (x, 0))
+            x += c.get_width()
+        return surface
+
+
+banner_font = BannerFont()
 
 FONT_SIZE = 15
 FONT_DIRECTORY = os.path.join("assets", "font", "PKMN-Mystery-Dungeon.ttf")
