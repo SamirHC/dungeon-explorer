2c867c55f90750ee7d2f17fc2ec6d87f04455964
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-04-01 00:21:44 +0100

    Update tileset.py

--- dungeon_explorer/dungeon/tileset.py
+++ dungeon_explorer/dungeon/tileset.py
@@ -11,31 +11,33 @@ STAIRS_DOWN_IMAGE = pygame.image.load(os.path.join("assets", "images", "stairs",
 STAIRS_UP_IMAGE = pygame.image.load(os.path.join("assets", "images", "stairs", "StairsUp.png"))
 SHOP_IMAGE = pygame.image.load(os.path.join("assets", "images", "traps", "KecleonCarpet.png"))
 
+def get_tile_masks() -> list[tile.TileMask]:
+    pattern_dir = os.path.join("assets", "images", "tilesets", "patterns.txt")
+    with open(pattern_dir) as f:
+        return [tile.TileMask(line) for line in f.read().splitlines()]
+
 
 class Tileset:
     TILE_SET_DIR = os.path.join("assets", "images", "tilesets")
+    tile_masks = get_tile_masks()
 
     def __init__(self, tileset_id: str):
         self.tileset_id = tileset_id
-        self.tile_masks = self.get_tile_masks()
-
         base_dir = os.path.join(Tileset.TILE_SET_DIR, tileset_id)
+        self.get_metadata(base_dir)
+        self.get_tileset(base_dir)
+        self.invalid_color = self[(5, 2), 0].get_at((0, 0))
 
+    def get_metadata(self, base_dir: str):
         self.metadata = ET.parse(os.path.join(base_dir, "tileset.dtef.xml")).getroot()
         self.tile_size = int(self.metadata.get("dimensions"))
-        self.animation_10_node, self.animation_11_node = self.metadata.findall("Animation")
-        self.is_animated_10 = bool(list(self.animation_10_node))
-        self.is_animated_11 = bool(list(self.animation_11_node))
+        animation_10_node, animation_11_node = self.metadata.findall("Animation")
+        self.is_animated_10 = bool(list(animation_10_node))
+        self.is_animated_11 = bool(list(animation_11_node))
         if self.is_animated_10:
-            self.animation_10 = self.get_animation_10()
+            self.animation_10 = animation.PaletteAnimation(animation_10_node)
         if self.is_animated_11:
-            self.animation_11 = self.get_animation_11()
-
-        self.tile_set: list[pygame.Surface] = []
-        for i in range(3):
-            self.tile_set.append(pygame.image.load(os.path.join(base_dir, f"tileset_{i}.png")))
-
-        self.invalid_color = self.tile_set[0].get_at((5*self.tile_size, 2*self.tile_size))
+            self.animation_11 = animation.PaletteAnimation(animation_11_node)
 
         self.gamedata = ET.parse(os.path.join(base_dir, "tileset_data.xml")).getroot()
         self.primary_type = tile.Terrain(self.gamedata.find("PrimaryType").text)
@@ -44,19 +46,10 @@ class Tileset:
         self.minimap_color = self.get_minimap_color()
         self.underwater = bool(int(self.gamedata.find("Underwater").text))
 
-    def get_tile_masks(self) -> list[tile.TileMask]:
-        pattern_dir = os.path.join("assets", "images", "tilesets", "patterns.txt")
-        with open(pattern_dir) as f:
-            return [tile.TileMask(line) for line in f.read().splitlines()]
-
-    def get_animation_10(self) -> animation.PaletteAnimation:
-        return self.get_animation(self.animation_10_node)
-
-    def get_animation_11(self) -> animation.PaletteAnimation:
-        return self.get_animation(self.animation_11_node)
-
-    def get_animation(self, node) -> animation.PaletteAnimation:
-        return animation.PaletteAnimation(node)
+    def get_tileset(self, base_dir):
+        self.tileset: list[pygame.Surface] = []
+        for i in range(3):
+            self.tileset.append(pygame.image.load(os.path.join(base_dir, f"tileset_{i}.png")))
 
     def get_terrain(self, tile_type: tile.TileType) -> tile.Terrain:
         if tile_type is tile.TileType.PRIMARY:
@@ -72,13 +65,13 @@ class Tileset:
 
     def __getitem__(self, position: tuple[tuple[int, int], int]) -> pygame.Surface:
         (x, y), v = position
-        return self.tile_set[v].subsurface((x*self.tile_size, y*self.tile_size), (self.tile_size, self.tile_size))
+        return self.tileset[v].subsurface((x*self.tile_size, y*self.tile_size), (self.tile_size, self.tile_size))
 
     def get_tile_position(self, tile_type: tile.TileType, pattern: tile.TileMask, variation: int=0) -> tuple[tuple[int, int], int]:
         return (self.get_position(tile_type, pattern), variation)
 
     def get_position(self, tile_type: tile.TileType, mask: tile.TileMask) -> tuple[int, int]:
-        for i, p in enumerate(self.tile_masks):
+        for i, p in enumerate(Tileset.tile_masks):
             if i == 17:
                 continue
             if p.matches(mask):
@@ -104,7 +97,7 @@ class Tileset:
         if not updated:
             return
 
-        for surf in self.tile_set:
+        for surf in self.tileset:
             if self.is_animated_10:
                 palette = self.animation_10.current_palette()
                 for i in range(16):
