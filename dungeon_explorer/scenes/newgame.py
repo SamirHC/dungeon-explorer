d33d2767785eee68e2e1a11c949fb82bd7adf2d6
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-04-10 14:19:14 +0100

    NewGameScene uses scroll text

--- dungeon_explorer/scenes/newgame.py
+++ dungeon_explorer/scenes/newgame.py
@@ -8,20 +8,31 @@ from dungeon_explorer.scenes import scene, dungeon
 class NewGameScene(scene.Scene):
     def __init__(self):
         super().__init__()
+        self.scroll_texts = [
+            text.ScrollText("Welcome!"),
+            text.ScrollText("This is the portal that leads to the\nworld inhabited only by Pokemon")
+        ]
+        self.index = 0
+    
+    @property
+    def current_text(self) -> text.ScrollText:
+        return self.scroll_texts[self.index]
 
     def process_input(self, input_stream: inputstream.InputStream):
         if input_stream.keyboard.is_pressed(pygame.K_RETURN):
-            entry_party = party.Party("0")
-            entry_party.add("3")
-            self.next_scene = dungeon.StartDungeonScene("14", entry_party)
+            if self.index != len(self.scroll_texts) - 1:
+                self.index += 1
+            else:
+                entry_party = party.Party("0")
+                entry_party.add("3")
+                self.next_scene = dungeon.StartDungeonScene("14", entry_party)
 
     def update(self):
-        pass
+        self.current_text.update()
 
     def render(self) -> pygame.Surface:
         surface = pygame.Surface(constants.DISPLAY_SIZE, pygame.SRCALPHA)
         surface.fill(constants.BLACK)
-        welcome_text = text.build("Welcome to Pokemon Mystery Dungeon Remake")
-        rect = welcome_text.get_rect(center=surface.get_rect().center)
-        surface.blit(welcome_text, rect.topleft)
+        rect = self.current_text.empty_surface.get_rect(center=surface.get_rect().center)
+        surface.blit(self.current_text.render(), rect.topleft)
         return surface
