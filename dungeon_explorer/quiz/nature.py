0f6e346b83cfb50d04a5af25bc50dcd754ecd8c5
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-04-11 17:27:42 +0100

    Nature enum

---
+++ dungeon_explorer/quiz/nature.py
@@ -0,0 +1,20 @@
+import enum
+
+
+class Nature(enum.Enum):
+    BOLD = 0
+    BRAVE = 1
+    CALM = 2
+    DOCILE = 3
+    HARDY = 4
+    HASTY = 5
+    IMPISH = 6
+    JOLLY = 7
+    LONELY = 8
+    NAIVE = 9
+    QUIET = 10
+    QUIRKY = 11
+    RASH = 12
+    RELAXED = 13
+    SASSY = 14
+    TIMID = 15
