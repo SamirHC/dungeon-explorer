9c79e01d8a8d8c3841c30f65ac2eb1b740da291c
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-04-12 16:31:56 +0100

    Fixed type hint

--- dungeon_explorer/quiz/questions.py
+++ dungeon_explorer/quiz/questions.py
@@ -8,7 +8,7 @@ class Question:
     def __init__(self, question: str):
         self.question = question
         self.options: list[str] = []
-        self.results: list[tuple[Nature, int]] = []
+        self.results: list[list[tuple[Nature, int]]] = []
 
     def add_option(self, option: str, result: list[tuple[Nature, int]]):
         self.options.append(option)
