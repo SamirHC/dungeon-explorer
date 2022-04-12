431fe46b9f72a030ebe34f85f8927ca73b3a5e85
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-04-12 16:15:37 +0100

    Split options and results

--- dungeon_explorer/quiz/questions.py
+++ dungeon_explorer/quiz/questions.py
@@ -7,10 +7,12 @@ from dungeon_explorer.quiz.nature import Nature
 class Question:
     def __init__(self, question: str):
         self.question = question
-        self.options = []
+        self.options: list[str] = []
+        self.results: list[tuple[Nature, int]] = []
 
     def add_option(self, option: str, result: list[tuple[Nature, int]]):
-        self.options.append((option, result))
+        self.options.append(option)
+        self.results.append(result)
 
 def load_questions() -> list[Question]:
     file = os.path.join("data", "gamedata", "quiz", "questions.xml")
