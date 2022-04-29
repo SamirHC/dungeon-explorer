9c68b8ccde3e86ad1306a42cfcf09bb8413305a5
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-04-29 16:35:15 +0100

    Update newgame.py

--- dungeon_explorer/scenes/newgame.py
+++ dungeon_explorer/scenes/newgame.py
@@ -18,24 +18,31 @@ class NewGameScene(scene.Scene):
     def __init__(self):
         super().__init__()
         self.scroll_texts = [
-            text.ScrollText("Welcome!"),
-            text.ScrollText("This is the portal that leads to the\nworld inhabited only by Pokemon.", text.Align.CENTER),
-            text.ScrollText("Beyond this gateway, many new\nadventures and fresh experiences\nawait your arrival!", text.Align.CENTER),
-            text.ScrollText("Before you depart for adventure,\nyou must answer some questions.", text.Align.CENTER),
-            text.ScrollText("Be truthful when you answer them!"),
-            text.ScrollText("Now, are you ready?"),
-            text.ScrollText("Then... let the questions begin!")
+            "Welcome!",
+            "This is the portal that leads to the\nworld inhabited only by Pokemon.",
+            "Beyond this gateway, many new\nadventures and fresh experiences\nawait your arrival!",
+            "Before you depart for adventure,\nyou must answer some questions.",
+            "Be truthful when you answer them!",
+            "Now, are you ready?",
+            "Then... let the questions begin!"
         ]
         self.index = 0
+        self.current_text = self.make_scroll_text(self.scroll_texts[self.index])
     
-    @property
-    def current_text(self) -> text.ScrollText:
-        return self.scroll_texts[self.index]
+    def make_scroll_text(self, message: str) -> text.ScrollText:
+        return text.ScrollText(
+            text.TextBuilder()
+            .set_alignment(text.Align.CENTER)
+            .set_color(constants.OFF_WHITE)
+            .write(message)
+            .build()
+        )
 
     def process_input(self, input_stream: inputstream.InputStream):
         if input_stream.keyboard.is_pressed(pygame.K_RETURN):
             if self.index != len(self.scroll_texts) - 1:
                 self.index += 1
+                self.current_text = self.make_scroll_text(self.scroll_texts[self.index])
             else:
                 self.next_scene = QuizScene()
 
@@ -45,7 +52,7 @@ class NewGameScene(scene.Scene):
     def render(self) -> pygame.Surface:
         surface = pygame.Surface(constants.DISPLAY_SIZE, pygame.SRCALPHA)
         surface.fill(constants.BLACK)
-        rect = self.current_text.empty_surface.get_rect(centerx=surface.get_rect().centerx, y=80)
+        rect = self.current_text.get_rect(centerx=surface.get_rect().centerx, y=80)
         surface.blit(self.current_text.render(), rect.topleft)
         return surface
 
