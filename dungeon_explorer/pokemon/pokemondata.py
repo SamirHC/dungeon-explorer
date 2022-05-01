1d8fb02be8c41443101f2633b155bf0f6c63a857
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-05-01 19:13:48 +0100

    PokemonStrings, StatsGrowth, LevelUpMoves

--- dungeon_explorer/pokemon/pokemondata.py
+++ dungeon_explorer/pokemon/pokemondata.py
@@ -3,6 +3,55 @@ import enum
 
 from dungeon_explorer.common.statistic import Statistic
 from dungeon_explorer.dungeon import damage_chart
+from dungeon_explorer.move import move
+
+
+@dataclasses.dataclass(frozen=True)
+class PokemonStrings:
+    name: str
+    category: str
+
+
+@dataclasses.dataclass(frozen=True)
+class StatsGrowth:
+    required_xp: tuple[int]
+    hp: tuple[int]
+    attack: tuple[int]
+    defense: tuple[int]
+    sp_attack: tuple[int]
+    sp_defense: tuple[int]
+
+    def get_required_xp(self, level: int):
+        return self.required_xp[level]
+
+    def get_hp(self, level: int):
+        return sum(self.hp[:level+1])
+
+    def get_attack(self, level: int):
+        return sum(self.attack[:level+1])
+
+    def get_defense(self, level: int):
+        return sum(self.defense[:level+1])
+
+    def get_sp_attack(self, level: int):
+        return sum(self.sp_attack[:level+1])
+
+    def get_sp_defense(self, level: int):
+        return sum(self.sp_defense[:level+1])
+
+
+@dataclasses.dataclass
+class LevelUpMoves:
+    levels: tuple[int]
+    move_ids: tuple[int]
+
+    def get_level_up_moves(self, level: int) -> list[move.Move]:
+        res = []
+        for lv, move_id in zip(self.levels, self.move_ids):
+            if lv > level:
+                break
+            res.append(move.load_move(move_id))
+        return res
 
 
 class PokemonStatus:
