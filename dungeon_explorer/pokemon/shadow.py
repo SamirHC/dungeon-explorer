7646c4a4562962cefc419f3acead1ba034303a5e
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-05-07 21:50:46 +0100

    Create shadow.py

---
+++ dungeon_explorer/pokemon/shadow.py
@@ -0,0 +1,56 @@
+import enum
+import os
+
+import pygame
+import pygame.image
+from dungeon_explorer.common import constants
+
+
+class ShadowSize(enum.Enum):
+    SMALL = 0
+    MEDIUM = 1
+    LARGE = 2
+
+
+_base_dir = os.path.join("assets", "images", "spriteshadow")
+
+
+def _get_shadows(name: str) -> pygame.Surface:
+    return pygame.image.load(os.path.join(_base_dir, name))
+
+
+_black_shadows = _get_shadows("black_shadows.png")
+_gold_shadows = _get_shadows("gold_shadows.png")
+
+
+def _get_small_black_shadow() -> pygame.Surface:
+    colors = pygame.PixelArray(_black_shadows)
+    colors.replace((255, 0, 0), constants.TRANSPARENT)
+    colors.replace((0, 255, 0), constants.BLACK)
+    colors.replace((0, 0, 255), constants.TRANSPARENT)
+    return colors.make_surface()
+
+
+def _get_medium_black_shadow() -> pygame.Surface:
+    colors = pygame.PixelArray(_black_shadows)
+    colors.replace((255, 0, 0), constants.BLACK)
+    colors.replace((0, 255, 0), constants.BLACK)
+    colors.replace((0, 0, 255), constants.TRANSPARENT)
+    return colors.make_surface()
+
+
+def _get_large_black_shadow() -> pygame.Surface:
+    colors = pygame.PixelArray(_black_shadows)
+    colors.replace((255, 0, 0), constants.BLACK)
+    colors.replace((0, 255, 0), constants.BLACK)
+    colors.replace((0, 0, 255), constants.BLACK)
+    return colors.make_surface()
+
+
+SMALL_BLACK = _get_small_black_shadow()
+MEDIUM_BLACK = _get_medium_black_shadow()
+LARGE_BLACK = _get_large_black_shadow()
+
+SMALL_GOLD = _gold_shadows.subsurface((0, 0), (24, 10))
+MEDIUM_GOLD = _gold_shadows.subsurface((0, 10), (24, 10))
+LARGE_GOLD = _gold_shadows.subsurface((0, 20), (24, 10))
