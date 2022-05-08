c004495d6e86dca1bccfcc31dcd2488b919fc485
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-05-08 11:32:56 +0100

    Refactor SpriteCollection, remove shadows

--- dungeon_explorer/pokemon/pokemonsprite.py
+++ dungeon_explorer/pokemon/pokemonsprite.py
@@ -1,77 +1,38 @@
 import dataclasses
-import enum
 import os
 import xml.etree.ElementTree as ET
 
 import pygame
 import pygame.image
-from dungeon_explorer.common import constants, direction
-from dungeon_explorer.pokemon import portrait
-
-
-class ShadowSize(enum.Enum):
-    SMALL = 0
-    MEDIUM = 1
-    LARGE = 2
-
-    def color(self) -> pygame.Color:
-        if self is ShadowSize.SMALL: return pygame.Color(0, 255, 0)
-        if self is ShadowSize.MEDIUM: return pygame.Color(255, 0, 0)
-        if self is ShadowSize.LARGE: return pygame.Color(0, 0, 255)
+from dungeon_explorer.common import direction
+from dungeon_explorer.pokemon import portrait, shadow
 
 
 @dataclasses.dataclass(frozen=True)
 class SpriteSheet:
     name: str
-    surface: pygame.Surface
+    sprites: dict[direction.Direction, tuple[pygame.Surface, ...]]
     size: tuple[int, int]
     durations: tuple[int]
-    shadow_surface: pygame.Surface
 
     @property
     def num_rows(self) -> int:
-        return self.surface.get_height() // self.size[1]
+        return len(self.sprites)
 
     def __len__(self):
         return len(self.durations)
 
-    def __getitem__(self, position: tuple[int, int]) -> pygame.Surface:
-        x, y = position
-        w, h = self.size
-        return self.surface.subsurface((x*w, y*h), self.size)
-
-    def get_sprite(self, direction: direction.Direction, index: int) -> pygame.Surface:
-        x = index
-        y = self.get_row(direction)
-        surface = pygame.Surface(self.size, pygame.SRCALPHA)
-        sprite = self[x, y]
-        shadow = self.get_shadow((x, y))
-        surface.blit(shadow, (0, 0))
-        surface.blit(sprite, (0, 0))
-        return surface
-
-    def get_shadow(self, position: tuple[int, int]) -> pygame.Surface:
-        x, y = position
-        w, h = self.size
-        return self.shadow_surface.subsurface((x*w, y*h), self.size)
-    
-    def get_row(self, d: direction.Direction) -> int:
-        if self.num_rows == 1:
-            return 0
-        if d is direction.Direction.SOUTH: return 0
-        if d is direction.Direction.SOUTH_EAST: return 1
-        if d is direction.Direction.EAST: return 2
-        if d is direction.Direction.NORTH_EAST: return 3
-        if d is direction.Direction.NORTH: return 4
-        if d is direction.Direction.NORTH_WEST: return 5
-        if d is direction.Direction.WEST: return 6
-        if d is direction.Direction.SOUTH_WEST: return 7
+    def get_sprite(self, d: direction.Direction, index: int) -> pygame.Surface:
+        if d not in self.sprites:
+            d = direction.Direction.SOUTH
+        return self.sprites[d][index]
 
 
 @dataclasses.dataclass(frozen=True)
 class SpriteCollection:
     sprite_sheets: dict[int, SpriteSheet]
     portraits: portrait.Portrait
+    shadow_size: shadow.ShadowSize
 
     def get_sprite(self, anim_id: int, direction: direction.Direction, index: int) -> pygame.Surface:
         sheet = self.sprite_sheets[anim_id]
@@ -149,35 +110,34 @@ class PokemonImageDatabase:
         sprite_dir = os.path.join(self.base_dir, str(dex))
 
         def _get_file(filename):
-            return os.path.join(sprite_dir, filename)
+            return os.path.join(sprite_dir, filename)            
 
-        def _load_shadow_sheet(path: str, shadow_size: ShadowSize) -> pygame.Surface:
-            colors = pygame.PixelArray(pygame.image.load(path))
-            colors.replace(constants.WHITE, constants.BLACK)
-            colors.replace(ShadowSize.SMALL.color(), constants.BLACK)
-            if shadow_size is ShadowSize.SMALL:
-                colors.replace(ShadowSize.MEDIUM.color(), constants.TRANSPARENT)
-                colors.replace(ShadowSize.LARGE.color(), constants.TRANSPARENT)
-            elif shadow_size is ShadowSize.MEDIUM:
-                colors.replace(ShadowSize.MEDIUM.color(), constants.BLACK)
-                colors.replace(ShadowSize.LARGE.color(), constants.TRANSPARENT)
-            elif shadow_size is ShadowSize.LARGE:
-                colors.replace(ShadowSize.MEDIUM.color(), constants.BLACK)
-                colors.replace(ShadowSize.LARGE.color(), constants.BLACK)
-            return colors.make_surface()
-
-        def _load_sprite_sheet(anim: ET.Element, shadow_size: ShadowSize) -> SpriteSheet:
+        def _load_sprite_sheet(anim: ET.Element) -> SpriteSheet:
             anim_name = anim.find("Name").text
-            anim_sheet = pygame.image.load(_get_file(f"{anim_name}-Anim.png"))
-            shadow_sheet = _load_shadow_sheet(_get_file(f"{anim_name}-Shadow.png"), shadow_size)
-            frame_size = (int(anim.find("FrameWidth").text), int(anim.find("FrameHeight").text))
+            frame_size = frame_w, frame_h = int(anim.find("FrameWidth").text), int(anim.find("FrameHeight").text)
             durations = tuple([int(d.text) for d in anim.find("Durations").findall("Duration")])
-            return SpriteSheet(anim_name, anim_sheet, frame_size, durations, shadow_sheet)
+            
+            anim_sheet = pygame.image.load(_get_file(f"{anim_name}-Anim.png"))
+            if frame_h == anim_sheet.get_height():
+                num_rows = 1
+            else:
+                num_rows = len(direction.Direction)
+            sprites: dict[direction.Direction, tuple[pygame.Surface, ...]] = {}
+            d = direction.Direction.SOUTH
+            for row in range(num_rows):
+                direction_sprites = []
+                for i in range(len(durations)):
+                    direction_sprite = anim_sheet.subsurface((frame_w*i, frame_h*row), frame_size)
+                    direction_sprites.append(direction_sprite)
+                sprites[d] = tuple(direction_sprites)
+                d = d.anticlockwise()
+
+            return SpriteSheet(anim_name, sprites, frame_size, durations)
         
         anim_data_file = _get_file("AnimData.xml")
         anim_root = ET.parse(anim_data_file).getroot()
 
-        shadow_size = ShadowSize(int(anim_root.find("ShadowSize").text))
+        shadow_size = shadow.ShadowSize(int(anim_root.find("ShadowSize").text))
 
         anims = anim_root.find("Anims").findall("Anim")
         sprite_sheets = {}
@@ -191,13 +151,14 @@ class PokemonImageDatabase:
                 for anim_ in anims:
                     if anim_.find("Name").text == copy_anim_name:
                         anim = anim_
-            sprite_sheets[index] = _load_sprite_sheet(anim, shadow_size)
+            sprite_sheets[index] = _load_sprite_sheet(anim)
 
         portraits = portrait.Portrait(dex)
 
         sprite_collection = SpriteCollection(
             sprite_sheets,
-            portraits
+            portraits,
+            shadow_size
         )
         self.loaded[dex] = sprite_collection
 
