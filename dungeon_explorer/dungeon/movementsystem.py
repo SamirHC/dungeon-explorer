28d6bf958bc46f5e5be79b8c6fdbd3066081ea62
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-05-12 17:45:13 +0100

    can_move takes direction
    Direction should only be changed when intending to stay that direction

--- dungeon_explorer/dungeon/movementsystem.py
+++ dungeon_explorer/dungeon/movementsystem.py
@@ -60,12 +60,12 @@ class MovementSystem:
         else:
             self.deactivate()
 
-    def can_move(self, p: pokemon.Pokemon) -> bool:
-        new_position = p.facing_position()
+    def can_move(self, p: pokemon.Pokemon, d: direction.Direction) -> bool:
+        new_position = p.x + d.x, p.y + d.y
         traversable = self.can_walk_on(p, new_position)
         unoccupied = not self.dungeon.is_occupied(new_position)
         not_corner = not self.dungeon.cuts_corner(
-            p.position, p.direction) or p.movement_type is pokemondata.MovementType.PHASING
+            p.position, d) or p.movement_type is pokemondata.MovementType.PHASING
         return traversable and unoccupied and not_corner
 
     def can_walk_on(self, p: pokemon.Pokemon, position: tuple[int, int]):
@@ -116,11 +116,11 @@ class MovementSystem:
         return False
     
     def input_move(self, input_stream: inputstream.InputStream) -> bool:
-        for key in self.direction_keys:
+        for key, d in self.direction_keys.items():
             if not(input_stream.keyboard.is_pressed(key) or input_stream.keyboard.is_held(key)):
                 continue
-            self.user.direction = self.direction_keys[key]
-            if self.can_move(self.user):
+            self.user.direction = d
+            if self.can_move(self.user, d):
                 self.add(self.user)
                 self.user.has_turn = False
                 return True
@@ -128,20 +128,24 @@ class MovementSystem:
 
     def ai_move(self, p: pokemon.Pokemon):
         self.update_ai_target(p)
+        if p.target == p.position:
+            return
         p.face_target(p.target)
-        if self.can_move(p):
+        if self.dungeon.is_occupied(p.target) and p.target == p.facing_position():
+            return
+        if self.can_move(p, p.direction):
             self.add(p)
             return
-        original_direction = p.direction
-        p.direction = original_direction.clockwise()
-        if self.can_move(p):
+        cw = p.direction.clockwise()
+        if self.can_move(p, cw):
+            p.direction = cw
             self.add(p)
             return
-        p.direction = original_direction.anticlockwise()
-        if self.can_move(p):
+        acw = p.direction.anticlockwise()
+        if self.can_move(p, acw):
+            p.direction = acw
             self.add(p)
             return
-        p.direction = original_direction  # Do nothing
 
     def tile_is_visible_from(self, observer: tuple[int, int], target: tuple[int, int]) -> bool:
         if abs(observer[0] - target[0]) <= 2:
@@ -166,19 +170,19 @@ class MovementSystem:
                 if p.target in self.dungeon.floor.room_exits[room_number]:
                     return
         # 4. Target corridor
-        possible_targets = []
+        possible_directions: list[direction.Direction] = []
         for d in direction.Direction:
-            p.direction = d
-            target = p.facing_position()
+            target = p.x + d.x, p.y + d.y
             if self.dungeon.floor.in_same_room(target, p.position):
                 continue
             if target == p.tracks[0]:
                 continue
-            if not self.can_move(p):
+            if not self.can_move(p, d):
                 continue
-            possible_targets.append(target)
-        if possible_targets:
-            p.target = random.choice(possible_targets)
+            possible_directions.append(d)
+        if possible_directions:
+            d = random.choice(possible_directions)
+            p.target = p.x + d.x, p.y + d.y
             return
         elif not self.dungeon.floor.is_room(p.position):
             p.target = p.tracks[0]
@@ -193,13 +197,12 @@ class MovementSystem:
                 p.target = p.tracks[0]
             return
         # 6. Random
-        possible_targets = []
+        possible_directions = []
         for d in list(direction.Direction):
-            p.direction = d
-            target = p.facing_position()
-            if not self.can_move(p):
+            if not self.can_move(p, d):
                 continue
-            possible_targets.append(target)
-        if possible_targets:
-            p.target = random.choice(possible_targets)
+            possible_directions.append(d)
+        if possible_directions:
+            d = random.choice(possible_directions)
+            p.target = p.x + d.x, p.y + d.y
             return
