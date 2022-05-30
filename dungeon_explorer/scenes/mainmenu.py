ccb3bdc274c6dde6de8981912f3d774febdd8799
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2022-05-30 12:45:15 +0100

    Update userdata

--- dungeon_explorer/scenes/mainmenu.py
+++ dungeon_explorer/scenes/mainmenu.py
@@ -58,7 +58,7 @@ class MainMenuScene(scene.Scene):
             if self.continue_game_menu.current_option == "Continue":
                 pygame.mixer.music.fadeout(500)
                 entry_party = party.Party([
-                    pokemon.UserPokemon(0),
+                    pokemon.UserPokemon(1),
                     pokemon.UserPokemon(3)
                 ])
                 self.next_scene = dungeon.StartDungeonScene(26, entry_party)
