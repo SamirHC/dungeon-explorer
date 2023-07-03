cac5e37d6b1e96223269e6d7d6ad0e2d2e20801f
Samir Chowdhury <samir1184.sc@gmail.com>
2023-07-04 00:12:39 +0100

    Separated hudcomponents

--- app/dungeon/hud.py
+++ app/dungeon/hud.py
@@ -1,57 +1,13 @@
-import os
-
 import pygame
-import pygame.display
-import pygame.draw
-import pygame.image
 from app.common import constants, text
 from app.dungeon import dungeon
 from app.pokemon import pokemon
+from app.guicomponents.hudcomponents import HudComponents
 
 
 HP_RED = pygame.Color(255, 125, 95)
 HP_GREEN = pygame.Color(40, 248, 48)
 ORANGE = pygame.Color(248, 128, 88)
-
-
-class HudComponents:
-    HUD_COMPONENTS_FILE = os.path.join("assets", "images", "misc", "hud_components.png")
-    SIZE = 8
-
-    def __init__(self):
-        self.hud_components = pygame.image.load(self.HUD_COMPONENTS_FILE)
-        self.hud_components.set_colorkey(self.hud_components.get_at((0, 0)))
-
-    def __getitem__(self, position: tuple[int, int]):
-        x, y = position
-        return self.hud_components.subsurface(x*self.SIZE, y*self.SIZE, self.SIZE, self.SIZE)
-
-    def get_f(self) -> pygame.Surface:
-        return self[10, 0]
-
-    def get_l(self) -> pygame.Surface:
-        return self[11, 0]
-
-    def get_v(self) -> pygame.Surface:
-        return self[12, 0]
-
-    def get_b(self) -> pygame.Surface:
-        return self[13, 1]
-
-    def get_h(self) -> pygame.Surface:
-        return self[10, 1]
-
-    def get_p(self) -> pygame.Surface:
-        return self[11, 1]
-
-    def get_slash(self) -> pygame.Surface:
-        return self[12, 1]
-
-    def get_white_number(self, n: int) -> pygame.Surface:
-        return self[n, 0]
-
-    def get_green_number(self, n: int) -> pygame.Surface:
-        return self[n, 1]
 
 
 class Hud:    
