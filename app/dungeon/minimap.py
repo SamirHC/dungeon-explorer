354829a70b165f02ff954016877da6e6f18cbc17
Samir Chowdhury <samir1184.sc@gmail.com>
2023-07-28 19:19:29 +0100

    minimap draws items

--- app/dungeon/minimap.py
+++ app/dungeon/minimap.py
@@ -16,20 +16,24 @@ class MiniMap:
     def build_surface(self):
         size = self.get_scaled(self.floor.SIZE)
         self.surface = pygame.Surface(size, pygame.SRCALPHA)
-        for pos in [(x, y) for x in range(self.floor.WIDTH) for y in range(self.floor.HEIGHT)]:
-            component = None
-            if self.floor.is_tertiary(pos):
-                component = self.components.get_ground(self.floor.get_cardinal_tile_mask(pos), pos in self.visible)
-            if pos in self.visible:
-                if self.floor.stairs_spawn == pos:
-                    component = self.components.stairs
-                elif self.floor[pos].trap is Trap.WONDER_TILE:
-                    component = self.components.wonder_tile
-                elif self.floor[pos].trap is not None:
-                    component = self.components.trap
-            if component is not None:
-                dest = self.get_scaled(pos)
-                self.surface.blit(component, dest)
+        for x in range(self.floor.WIDTH):
+            for y in range(self.floor.HEIGHT):
+                pos = x, y
+                component = None
+                if self.floor.is_tertiary(pos):
+                    component = self.components.get_ground(self.floor.get_cardinal_tile_mask(pos), pos in self.visible)
+                if pos in self.visible:
+                    if self.floor.stairs_spawn == pos:
+                        component = self.components.stairs
+                    elif self.floor[pos].trap is Trap.WONDER_TILE:
+                        component = self.components.wonder_tile
+                    elif self.floor[pos].trap is not None:
+                        component = self.components.trap
+                    elif self.floor[pos].item_ptr is not None:
+                        component = self.components.item
+                if component is not None:
+                    dest = self.get_scaled(pos)
+                    self.surface.blit(component, dest)
         return self.surface
 
     def set_visible(self, position: tuple[int, int]):
