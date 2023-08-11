3dc4e04cab799c4ab591b3a38bb814bd71505825
Samir Chowdhury <samir1184.sc@gmail.com>
2023-08-11 03:07:00 +0100

    ActionEvent

--- app/events/event.py
+++ app/events/event.py
@@ -1,5 +1,7 @@
 import pygame
 
+from app.common.action import Action
+
 TOGGLE_FULLSCREEN_EVENT = pygame.USEREVENT + 1
 
 
@@ -11,3 +13,8 @@ class SleepEvent(Event):
     def __init__(self, time: int):
         super().__init__()
         self.time = time
+
+
+class ActionEvent(Event):
+    def __init__(self, action: Action):
+        self.action = action
