99c173cee4877e64d0b7810ec6b749104700fcd1
Samir Chowdhury <samir1184.sc@gmail.com>
2023-08-11 16:46:06 +0100

    working on event system

---
+++ app/events/eventhandler.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+from app.events.event import Event
+
+class EventHandler(ABC):
+    @abstractmethod
+    def handle_event(self, event: Event):
+        pass
---
+++ app/events/eventmanager.py
@@ -0,0 +1,20 @@
+from app.events.event import Event
+
+class EventManager:
+    def __init__(self):
+        self.listeners: dict[Event, list]  = {}
+
+    def add_listener(self, event_type, listener):
+        if event_type in self.listeners:
+            self.listeners[event_type].append(listener)
+        else:
+            self.listeners[event_type] = [listener]
+
+    def remove_listener(self, event_type, listener):
+        if event_type in self.listeners and listener in self.listeners[event_type]:
+            self.listeners[event_type].remove(listener)
+
+    def dispatch_event(self, event_type, data=None):
+        if event_type in self.listeners:
+            for listener in self.listeners[event_type]:
+                listener.handle_event(data)
