314d6911588f970213bc97f31db97f48b6cabf00
Samir Chowdhury <samir1184.sc@gmail.com>
2023-11-26 18:15:55 +0000

    feat(main): Event handler dispatcher

--- main.py
+++ main.py
@@ -31,6 +31,11 @@ class Game():
         self.scene = mainmenu.MainMenuScene()
         self.init_display()
 
+        self.event_handler_dispatcher = {
+            pygame.QUIT: self.handle_quit,
+            event.TOGGLE_FULLSCREEN_EVENT: self.handle_toggle_fullscreen
+        }
+
     def init_display(self):
         self.display = display
         pygame.display.set_caption(constants.CAPTION)
@@ -65,17 +70,20 @@ class Game():
         self.scene.update()
 
         for ev in pygame.event.get():
-            if ev.type == pygame.QUIT:
-                self.running = False
-            elif ev.type == event.TOGGLE_FULLSCREEN_EVENT:
-                flags = self.display.get_flags()
-                if flags & pygame.FULLSCREEN:
-                    flags &= ~pygame.FULLSCREEN
-                else:
-                    flags |= pygame.FULLSCREEN
-                pygame.display.set_mode(constants.DISPLAY_SIZE, flags)
+            self.event_handler_dispatcher.get(ev.type, lambda: None)()
 
         self.clock.tick(constants.FPS)
+
+    def handle_quit(self):
+        self.running = False
+    
+    def handle_toggle_fullscreen(self):
+        flags = self.display.get_flags()
+        if flags & pygame.FULLSCREEN:
+            flags &= ~pygame.FULLSCREEN
+        else:
+            flags |= pygame.FULLSCREEN
+        pygame.display.set_mode(constants.DISPLAY_SIZE, flags)
 
     def render(self):
         self.display.fill(constants.BLACK)
