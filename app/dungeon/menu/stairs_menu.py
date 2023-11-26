a4b15c34614d0dc8f845e7f5d3643fd5d2c3617e
Samir Chowdhury <samir1184.sc@gmail.com>
2023-11-26 19:02:01 +0000

    refactor(dungeon): Separate dungeon menus

---
+++ app/dungeon/menu/stairs_menu.py
@@ -0,0 +1,41 @@
+from app.common import constants, menu, text
+from app.common.inputstream import InputStream
+from app.model.frame import Frame
+
+
+import pygame
+
+
+class StairsMenu:
+    def __init__(self):
+        self.menu = menu.Menu((9, 8), ["Proceed", "Info", "Cancel"], 128)
+        self.frame = self.build_stairs_surface()
+        self.auto = False
+        self.proceed = False
+        self.cancelled = True
+
+    def build_stairs_surface(self) -> pygame.Surface:
+        surface = Frame((21, 6), 128).with_header_divider()
+        stairs_text_surface = (
+            text.TextBuilder()
+            .set_shadow(True)
+            .set_color(text.WHITE)
+            .write("Stairs")
+            .build()
+            .render()
+        )
+        surface.blit(stairs_text_surface, (16, 10))
+        surface.blit(stairs_text_surface, (24, 28))
+        return surface
+
+    def process_input(self, input_stream: InputStream):
+        self.menu.process_input(input_stream)
+
+    def update(self):
+        self.menu.update()
+
+    def render(self) -> pygame.Surface:
+        surface = pygame.Surface(constants.DISPLAY_SIZE, pygame.SRCALPHA)
+        surface.blit(self.frame, (8, 8))
+        surface.blit(self.menu.render(), (176, 8))
+        return surface
