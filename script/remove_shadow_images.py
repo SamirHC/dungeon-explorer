f4f4a03247c2cbdb9cb7780c32c9afaa88f83a67
Samir Chowdhury <samir1184.sc@gmail.com>
2023-12-13 01:39:20 +0000

    Remove offsets images

--- script/remove_shadow_images.py
+++ script/remove_shadow_images.py
@@ -7,7 +7,7 @@ def main():
     for dex in range(896):
         try:
             dex_dir = os.path.join(constants.IMAGES_DIRECTORY, "sprites", str(dex))
-            to_remove = os.path.join(dex_dir, f"*Shadow.png")
+            to_remove = os.path.join(dex_dir, f"*Offsets.png")
             for path in glob.glob(to_remove):
                 os.remove(path)
         except:
