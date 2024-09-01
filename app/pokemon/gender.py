ec2e993ce5534778e3e1978f6a66f0a3aa6d7c6e
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2024-09-01 18:13:11 +0100

    Add gender to enemy name

--- app/pokemon/gender.py
+++ app/pokemon/gender.py
@@ -6,3 +6,12 @@ class Gender(Enum):
     MALE = 1
     FEMALE = 2
     GENDERLESS = 3
+
+    def get_font_string(self) -> list[str]:
+        match self:
+            case Gender.MALE:
+                return [chr(189)]
+            case Gender.FEMALE:
+                return [chr(190)]
+            case _:
+                return []
