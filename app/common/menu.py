54e8d84aa686fdcdb438dfc304882bc2c072087f
Samir Hasan Chowdhury <shc220@ic.ac.uk>
2025-09-22 12:40:42 +0100

    Create new menu classes

--- app/common/menu.py
+++ app/common/menu.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from typing import Any
+
 import pygame
 
 from app.common.action import Action
@@ -6,6 +9,98 @@ from app.common import settings
 from app.gui.frame import Frame
 import app.db.database as db
 from app.gui import text
+
+
+class MenuOption:
+    def __init__(self, label: Any, enabled: bool = True, metadata: dict | None = None):
+        self.label = label
+        # Menu assigned automatically on MenuPage.add_option call
+        self.menu: MenuPage | None = None
+        self.child_menu: MenuPage | None = None
+        self.enabled = enabled
+        self.metadata = metadata or {}
+
+    def set_child_menu(self, child_menu: MenuPage):
+        self.child_menu = child_menu
+        child_menu.parent_menu = self.menu
+
+
+class MenuPage:
+    def __init__(self, label: Any):
+        self.label = label
+        self.options: list[MenuOption] = []
+        # Parent menu set automatically on MenuOption.set_child_menu call
+        self.parent_menu: MenuPage | None = None
+        self.prev_page: MenuPage | None = None
+        self.next_page: MenuPage | None = None
+        self.pointer: int = 0
+
+    @property
+    def current_option(self) -> MenuOption:
+        return self.options[self.pointer]
+
+    def next(self):
+        self.pointer = (self.pointer + 1) % len(self.options)
+
+    def prev(self):
+        if len(self.options) <= 1:
+            return
+        self.pointer = (self.pointer - 1) % len(self.options)
+    
+    def add_option(self, menu_option: MenuOption):
+        menu_option.menu = self
+        self.options.append(menu_option)
+
+    @staticmethod
+    def connect_pages(*pages: MenuPage):
+        for i in range(len(pages) - 1):
+            current_page = pages[i]
+            next_page = pages[i + 1]
+            current_page.next_page = next_page
+            next_page.prev_page = current_page
+
+        first, last = pages[0], pages[-1]
+        first.prev_page = last
+        last.next_page = first
+
+
+class MenuController:
+    def __init__(self, start: MenuPage):
+        self.current_page = start
+        self.intent = None
+
+    @property
+    def current_option(self) -> MenuOption:
+        return self.current_page.current_option
+
+    def next(self):
+        self.current_page.next()
+
+    def prev(self):
+        self.current_page.prev()
+    
+    def next_page(self):
+        self.current_page = self.current_page.next_page or self.current_page
+        
+    def prev_page(self):
+        self.current_page = self.current_page.prev_page or self.current_page
+
+    def select(self):
+        option = self.current_option
+        if not option.enabled:
+            return
+        if option.child_menu:
+            self.current_page = option.child_menu
+            self.pointer = 0
+        else:
+            self.intent = option.label
+
+    def back(self):
+        self.current_page = self.current_page.parent_menu or self.current_page
+
+    def consume_intent(self):
+        intent, self.intent = self.intent, None
+        return intent
 
 
 class MenuModel:
@@ -39,6 +134,55 @@ class PagedMenuModel(MenuModel):
         self.page = (self.page - 1) % len(self.pages)
         self.options = self.pages[self.page]
         self.pointer = 0
+
+
+class MenuRenderer:
+    def __init__(
+        self,
+        size: tuple[int, int],
+        menu_page: MenuPage,
+        alpha=255,
+        header=False,
+        footer=False,
+        title: text.Text = None,
+    ):
+        self.textbox_frame = Frame(size, alpha)
+        self.header = header
+        self.footer = footer
+        if header:
+            self.textbox_frame.with_header_divider()
+            if title:
+                self.textbox_frame.blit(title.render(), (8, 8))
+        if footer:
+            self.textbox_frame.with_footer_divider()
+        self.menu_page = menu_page
+        self.pointer_animation = db.get_pointer_animation()
+
+    def update(self):
+        self.pointer_animation.update()
+
+    def render(self) -> pygame.Surface:
+        surface = self.textbox_frame.copy()
+        x, y = self.textbox_frame.container_rect.topleft
+        y += 2
+        if self.header:
+            y += 16
+        dx = db.get_pointer().get_width() + 1
+        dy = db.get_pointer().get_height() + 2
+        for i, option in enumerate(self.menu_page.options):
+            if i == self.menu_page.pointer:
+                surface.blit(self.pointer_animation.get_current_frame(), (x, y))
+            surface.blit(self.render_option(option), (x + dx, y))
+            y += dy
+
+        return surface
+
+    def render_option(self, option: MenuOption) -> pygame.Surface:
+        color = text.WHITE if option.enabled else text.RED
+        return text.TextBuilder.build_color(color, option).render()
+
+
+
 
 
 class Menu:
---
+++ test/unit_test/common/test_menu.py
@@ -0,0 +1,137 @@
+import pytest
+
+from app.common.menu import MenuController, MenuOption, MenuPage
+
+
+@pytest.fixture(scope="function")
+def simple_menu() -> MenuPage:
+    menu = MenuPage(
+        label=0,
+    )
+    menu.add_option(MenuOption("0-0"))
+    menu.add_option(MenuOption("0-1"))
+    menu.add_option(MenuOption("0-2"))
+
+    return menu
+
+
+@pytest.fixture(scope="function")
+def complex_menu() -> MenuPage:
+    """
+         0 [0, 1]
+         1 [0 [0, 1, 2X, 3], 1]
+    """
+    # Page 0
+    page0 = MenuPage("0")
+    page0.add_option(opt0_0 := MenuOption("0-0"))
+    page0.add_option(opt0_1 := MenuOption("0-1"))
+
+    # Page 1
+    page1 = MenuPage("1")
+    page1.add_option(opt1_0 := MenuOption("1-0"))
+    page1.add_option(opt1_1 := MenuOption("1-1"))
+
+    # Page 1-0
+    page1_0 = MenuPage("1-0")
+    page1_0.add_option(opt1_0_0 := MenuOption("1-0-0"))
+    page1_0.add_option(opt1_0_1 := MenuOption("1-0-1"))
+    page1_0.add_option(opt1_0_2 := MenuOption("1-0-2", enabled=False))
+    page1_0.add_option(opt1_0_3 := MenuOption("1-0-3"))
+
+    # Connect
+    MenuPage.connect_pages(page0, page1)
+    opt1_0.set_child_menu(page1_0)
+    
+    return page0
+
+
+def test_simple_menu_next_navigation(simple_menu: MenuPage):
+    controller = MenuController(simple_menu)
+    assert controller.current_option.label == "0-0"
+    controller.next()
+    assert controller.current_option.label == "0-1"
+    controller.next()
+    assert controller.current_option.label == "0-2"
+    controller.next()
+    assert controller.current_option.label == "0-0"
+
+
+def test_simple_menu_prev_navigation(simple_menu: MenuPage):
+    controller = MenuController(simple_menu)
+    assert controller.current_option.label == "0-0"
+    controller.prev()
+    assert controller.current_option.label == "0-2"
+    controller.prev()
+    assert controller.current_option.label == "0-1"
+    controller.prev()
+    assert controller.current_option.label == "0-0"
+
+
+def test_simple_menu_selection(simple_menu: MenuPage):
+    controller = MenuController(simple_menu)
+    assert controller.intent is None
+    controller.select()
+    assert controller.intent == "0-0"
+
+    controller.next()
+    controller.select()
+    assert controller.intent == "0-1"
+
+
+def test_simple_menu_next_page(simple_menu: MenuPage):
+    controller = MenuController(simple_menu)
+    controller.next_page()
+    assert controller.current_option.label == "0-0"
+
+
+def test_complex_menu_next_navigation(complex_menu: MenuPage):
+    controller = MenuController(complex_menu)
+    assert controller.current_option.label == "0-0"
+    controller.next()
+    assert controller.current_option.label == "0-1"
+    controller.next()
+    assert controller.current_option.label == "0-0"
+
+
+def test_complex_menu_prev_navigation(complex_menu: MenuPage):
+    controller = MenuController(complex_menu)
+    assert controller.current_option.label == "0-0"
+    controller.prev()
+    assert controller.current_option.label == "0-1"
+    controller.prev()
+    assert controller.current_option.label == "0-0"
+
+
+def test_complex_menu_selection(complex_menu: MenuPage):
+    controller = MenuController(complex_menu)
+    controller.next_page()
+    controller.select()
+    assert controller.intent is None
+    assert controller.current_page.label == "1-0"
+
+    controller.next()
+    controller.select()
+    assert controller.intent == "1-0-1"
+    controller.consume_intent()
+
+    controller.next()
+    controller.select()
+    assert controller.current_option.label == "1-0-2"
+    assert controller.intent is None
+
+    controller.next()
+    controller.select()
+    assert controller.intent == "1-0-3"
+
+
+def test_complex_menu_parent_child_traversal(complex_menu: MenuPage):
+    controller = MenuController(complex_menu)
+    controller.next_page()
+    controller.select()
+    controller.back()
+    assert controller.current_page.label == "1"
+
+
+if __name__ == "__main__":
+    import sys
+    pytest.main(sys.argv)
